50d1417ae544c8782ef7a91d9af253b1a5503f6d
flashjpr <flashjpr@gmail.com>
2016-11-25 01:22:29 +0000

    Added tests for finding by id, handling error (for find) and the service implementation

--- spec/omdb/service.spec.js
+++ spec/omdb/service.spec.js
@@ -6,17 +6,66 @@ describe('omdb service', function () {
     var starWarsId = 'tt0076759';
     var movieDataById = {"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.","Language":"English","Country":"USA","Awards":"Won 6 Oscars. Another 48 wins & 28 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BZGEzZTExMDEtNjg4OC00NjQxLTk5NTUtNjRkNjA3MmYwZjg1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg","Metascore":"92","imdbRating":"8.7","imdbVotes":"927,743","imdbID":"tt0076759","Type":"movie","Response":"True"};
     var omdbApi = {};
+    var $httpBackend;
 
     beforeEach(module('omdb'));
-    beforeEach(inject(function (_omdbApi_) {
+    beforeEach(inject(function (_omdbApi_, _$httpBackend_) {
         omdbApi = _omdbApi_;
+        $httpBackend = _$httpBackend_;
     }));
 
     it('should return search movie data', function () {
-        expect(omdbApi.search('star wars')).toEqual(movieData);
+        var response;
+        var expectedUrl = 'http://www.omdbapi.com/?v=1&s=star%20wars';
+
+        $httpBackend.when('GET', expectedUrl)
+            .respond(200, movieData);
+
+        omdbApi.search('star wars')
+            .then(function (data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(movieData);
+    });
+
+    it('should handle error when a movie id is not found', function () {
+
+        var response;
+
+        $httpBackend.expect('GET', 'http://www.omdbapi.com/?v=1&i=tt0076759')
+            .respond(500);
+
+        omdbApi.find(starWarsId)
+            .then(function (data) {
+                response = data;
+            })
+            .catch(function () {
+                response = 'Error!';
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual('Error!');
     });
 
     it('should return movie data by id', function () {
-        expect(omdbApi.find(starWarsId)).toEqual(movieDataById);
+
+        var response;
+        var expectedUrl = 'http://www.omdbapi.com/?v=1&i=tt0076759';
+
+        $httpBackend.expect('GET', expectedUrl)
+            .respond(200, movieDataById );
+
+        omdbApi.find('tt0076759')
+            .then(function (data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(movieDataById);
     })
 });
--- src/omdb/service.js
+++ src/omdb/service.js
@@ -2,15 +2,28 @@
  * Created by flash on 23/11/2016.
  */
 angular.module('omdb', [])
-    .factory('omdbApi', function() {
+    .factory('omdbApi', function($http,$q) {
       var service = {};
+      var baseUrl = 'http://www.omdbapi.com/?v=1&';
+
+      function httpPromise(url) {
+          var deffered = $q.defer();
+          $http.get(url)
+              .success(function (data) {
+                  deffered.resolve(data);
+              })
+              .error(function () {
+                  deffered.reject();
+              });
+          return deffered.promise;
+      }
 
       service.search = function(query) {
-        return {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMWJhYWQ3ZTEtYTVkOS00ZmNlLWIxZjYtODZjNTlhMjMzNGM2XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_SX300.jpg","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"362","imdbID":"tt0251413","Type":"game","Response":"True"};
+          return httpPromise(baseUrl + 's=' + encodeURIComponent(query))
       };
 
       service.find = function(id) {
-        return {"Title":"Star Wars: Episode IV - A New Hope","Year":"1977","Rated":"PG","Released":"25 May 1977","Runtime":"121 min","Genre":"Action, Adventure, Fantasy","Director":"George Lucas","Writer":"George Lucas","Actors":"Mark Hamill, Harrison Ford, Carrie Fisher, Peter Cushing","Plot":"Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.","Language":"English","Country":"USA","Awards":"Won 6 Oscars. Another 48 wins & 28 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BZGEzZTExMDEtNjg4OC00NjQxLTk5NTUtNjRkNjA3MmYwZjg1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg","Metascore":"92","imdbRating":"8.7","imdbVotes":"927,743","imdbID":"tt0076759","Type":"movie","Response":"True"};
+          return httpPromise(baseUrl + 'i=' + id);
       };
 
       return service;
