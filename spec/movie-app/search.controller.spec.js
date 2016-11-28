71531dddd6a5f039b6a505770eddfc8692845fb3
flashjpr <flashjpr@gmail.com>
2016-11-28 03:32:06 +0000

    Used a timeout to update the movie query everry 1s; Used ng-mocks $timeout decorator of angular $timeout to be smart about unit testing the timeout

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -5,12 +5,14 @@ describe('Search Controller', function () {
     var $scope = {};
     var $location = {};
     var $controller;
+    var $timeout;
 
     beforeEach(module('movieApp'));
 
-    beforeEach(inject(function (_$controller_, _$location_) {
+    beforeEach(inject(function (_$controller_, _$location_, _$timeout_) {
         $controller = _$controller_
         $location = _$location_;
+        $timeout = _$timeout_;
     }));
 
     it('should redirect to the query results page for a non-empty query', function () {
@@ -23,5 +25,29 @@ describe('Search Controller', function () {
         $this = $controller('SearchController', {$location: $location}, {query:''});
         $this.search();
         expect($location.url()).toBe('');
+    });
+
+    it('should redirect after 1 second of keyboard interrupt', function () {
+        $this = $controller('SearchController', {$location: $location, $timeout:$timeout}, {query:'star wars'});
+        $this.keyup();
+        $timeout.flush();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+        expect($location.url()).toBe('/results?q=star%20wars');
+    });
+
+    it('should cancel timeout in keydown', function () {
+        $this = $controller('SearchController', {$location: $location, $timeout:$timeout}, {query:'star wars'});
+        $this.keyup();
+        $this.keydown();
+
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+    });
+
+    it('should cancel timeout on search', function () {
+        $this = $controller('SearchController', {$location: $location, $timeout:$timeout}, {query:'star wars'});
+        $this.keyup();
+        $this.search();
+
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
     });
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -8,10 +8,20 @@
         .module('movieApp')
         .controller('SearchController', SearchController);
 
-    SearchController.$inject = ['$location'];
+    SearchController.$inject = ['$location', '$timeout'];
 
-    function SearchController($location) {
+    function SearchController($location, $timeout) {
+        var vm = this;
+        var timeout;
+        this.keyup = function () {
+            timeout = $timeout(vm.search(), 1000);
+        };
+        
+        this.keydown = function () {
+            $timeout.cancel(timeout);
+        };
         this.search = function () {
+            $timeout.cancel(timeout);
             if(this.query) {
                 $location.path('/results').search('q', this.query);
             }
